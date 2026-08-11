3fab5294a5d6ea12c71cf1744fc5417b22377eff
Montana <a.presovsky@list.ru>
2026-08-11 14:05:17 +0300

    main 🌶️ feat: rs-pack migration

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
 import type { NextConfig } from 'next'
 
+import withRspack from 'next-rspack'
+import { PHASE_PRODUCTION_SERVER, type PHASE_TYPE } from 'next/constants'
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://juniorsbootcamp.ru/api/**')]
   },
   reactCompiler: true,
-  reactStrictMode: false,
-  turbopack: {
-    root: process.cwd()
-  }
+  reactStrictMode: false
 }
 
-export default nextConfig
+export default (phase: PHASE_TYPE): NextConfig =>
+  phase === PHASE_PRODUCTION_SERVER ? nextConfig : withRspack(nextConfig)
